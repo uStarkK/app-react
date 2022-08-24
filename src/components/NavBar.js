a33b9015fb7bf0f9249410af25b660a693e09932
Nahuel Garnica <nahugarnica@hotmail.com>
2022-08-24 18:16:04 -0300

    Creado NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+
+const NavBar = () => {
+    return (
+        <div className="NavBar">
+            <div className="navBarElement"><img className="logo" src="logo192.png" alt="logo"/></div>
+            <div className="navBarElement"><a href="">Inicio</a></div>
+            <div className="navBarElement"><a href="">Productos</a></div>
+            <div className="navBarElement"><a href="#">Nosotros</a></div>
+            <div className="navBarElement"><a href="#">Contacto</a></div>
+            <div className="navBarElement"><a href="#">LogIn</a></div>
+        </div>
+    )
+}
+
+export default NavBar
