4f03b4b40ce2e7d495bb8c7fc3ed0cbafbaf76ac
Nahuel Garnica <nahugarnica@hotmail.com>
2022-09-13 23:32:36 -0300

    commit

--- src/components/Logic/ItemDetail.js
+++ src/components/Logic/ItemDetail.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const ItemDetail = ({product}) => {
   return (
-    <div>Tu producto es {product.producto}    
+    <div className='productos'>
+            <div>{product.producto}</div>
+            <div>Marca: {product.marca}</div>
+            <div>
+                <img src={product.picURL} alt={product.producto}></img>
+            </div>
+            <div>Cantidad en stock: {product.stock}</div>
+            <div>Precio: ${product.precio} </div>
     </div> 
 
   )
